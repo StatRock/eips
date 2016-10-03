6b1fff5a2615f5726514e7929cf3ae9b7bd1e338
Marty Gentillon <marty.gentillon@gmail.com>
2016-10-02 19:00:33 -0700

    added metadata key checking

--- src/site/core.clj
+++ src/site/core.clj
@@ -1,26 +1,41 @@
 (ns site.core
   (:require [hiccup.page :as hp]
             [io.perun.core :as perun]
-            [boot.util :as u]))
+            [boot.util :as u]
+            [clojure.set :as set]))
 
-(def expected-keys [:content :extension :file-type
-                    :filename :full-path :mime-type
-                    :original :parent-path :path :short-filename
-                    :style :title])
-(def required-keys [:title])
+(def perun-keys #{:content :extension :file-type
+                :filename :full-path :mime-type
+                :original :parent-path :path :short-filename})
+(def known-yaml-keys #{:style :title})
 
-(def ensure-has-keys [data expected])
+(def required-yaml-keys  #{:title})
+
+(def permitted-default-template-keys
+  (set/union perun-keys known-yaml-keys))
+
+(defn enforce-has-keys [data expected]
+  (let [missing-keys (set/difference expected (set (keys data)))]
+    (if-not (empty? missing-keys)
+      (u/fail (str "Warning file: '" (:path data) "' is missing the following required metadata keys: " missing-keys "\n")))))
+
+(defn enforce-only-permitted-keys [data permitted]
+  (let [unexpected-keys (set/difference (set (keys data)) permitted)]
+    (if-not (empty? unexpected-keys)
+      (u/fail (str "Encountered unknown metadata keys in '" (:path data) "': " unexpected-keys)))))
 
 (defn template [data]
+  (enforce-has-keys data required-yaml-keys)
+  (enforce-only-permitted-keys data permitted-default-template-keys)
   (hp/html5
     [:div
-     (-> data :entry :content)]))
+     (:content data)]))
 
 (defn no-template [data]
-  (-> data :entry :content))
+  (enforce-only-permitted-keys data (set/union perun-keys #{:template}))
+  (:content data))
 
-(defn page [data]
+(defn page [{data :entry}]
   (clojure.pprint/pprint data)
-  (case (or (-> data :entry :template) :default)
-    :default (template data))
-  )
+  (case (or (:template data) :default)
+    :default (template data)))
