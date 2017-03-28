16429afef9fa6c132359af87549c50f5b8eb2a4b
Marty Gentillon <marty.gentillon@gmail.com>
2017-03-28 14:38:39 -0700

    Working on advancements
    It is going to be easier to use a JS lightbox library than to build
    static pages for paginating images.  If thumbnails are needed we can
    generate them during build.  (This is unlikely, as for most browsers,
    the lightbox library should handle that nicely.  The only time that
    we would really care is if we care about behavior on old browsers,
    and it's possible that even there, a single page filled with full
    size images might work well enough.)
    
    The advancement generation code here is initial.  I haven't gotten
    it to build the collections correctly, or adjusted the template
    to render them in any reasonable way.  This is still going to need
    some more work.  Because of this, advancements is still disabled in
    build.boot.

--- src/site/advancements.clj
+++ src/site/advancements.clj
@@ -1,13 +1,38 @@
 (ns site.advancements
-  (:require [io.perun :as perun-io]
+  (:require [io.perun :as perun]
             [boot.core :as boot :refer [deftask]]
-            [io.perun.core   :as perun]
+            [io.perun.core :as perun-core]
+            [boot.util :as u]
             [clojure.java.io :as io]
-            [clojure.string  :as str]
-            [clj-yaml.core   :as yaml]
-            [clojure.walk    :as walk]))
+            [clojure.string :as str]
+            [clj-yaml.core :as yaml]
+            [clojure.walk :as walk]
+            [selmer.parser :as selmer]
+            [clojure.string :as string]))
+
+(defn- normalize-path [path]
+  (string/replace path "\\" "/"))
+
+(defn- render-assortment [{data :entry}]
+  (u/info (clojure.pprint/write data :stream nil))
+  #_(selmer/render-file "advancement/advancement_indexes.html_template" data))
+
+(defn- advancement-path [file-name]
+  (let [path-parts (str/split "/" (normalize-path file-name))]
+    (str (first path-parts) "/" (second path-parts) "/index.html")))
+
+(defn- advancement-name [file-name]
+  (second (str/split "/" (normalize-path file-name))))
+
 
 (deftask advancements
-  "handles processing of the advancement images and templates"
+  "Generates advancement pages from the advancement images stored in content/advancements using
+  templates in templates/advancements.  The output from this task should then be rendered using
+  the default rendering template."
   []
-  )
+  (comp
+    #_(perun/assortment
+      :extensions [".jpg" ".png" ".jpeg"]
+      :filterer #(re-find #"^/advancements/" (normalize-path (:path %)))
+      :grouper #(advancement-path (:path %))        ; this needs to be a reducer... too bad that there isn't just a key function....
+      :renderer 'site.advancements/render-assortment)))
