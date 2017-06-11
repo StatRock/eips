ac6679acda3e802501bec8ff291935327b40c228
StatRock <cdg@cableone.net>
2017-06-11 01:06:18 -0600

    Using machine wrapping rather than manual.

--- src/site/advancements.clj
+++ src/site/advancements.clj
@@ -11,9 +11,7 @@
     (str (first path-parts) "/" (second path-parts) "/index.html")))
 
 (deftask advancements
-  "Generates advancement pages from the advancement images stored in content/advancements using
-  templates in templates/advancements.  The output from this task should then be rendered using
-  the default rendering template."
+  "Generates advancement pages from the advancement images stored in content/advancements using  templates in templates/advancements.  The output from this task should then be rendered using the default rendering template."
   []
   (comp
     (perun/assortment
